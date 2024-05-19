435762439ccfb6beae4c10bda9c69c9f4a2049bf
grro <gregor.roth@1und1.de>
2024-05-19 08:42:02 +0200

    initial check in

---
+++ presence.py
@@ -0,0 +1,97 @@
+import logging
+from threading import Thread
+from time import sleep
+from datetime import datetime, timedelta
+from abc import ABC, abstractmethod
+from typing import List
+from scapy.all import IP, ICMP, sr
+
+
+
+class Presence(ABC):
+
+    def __init__(self, name: str, addr: str):
+        self.name = name
+        self.addr = addr
+        self.__listeners = set()
+
+    def add_listener(self, listener):
+        self.__listeners.add(listener)
+
+    @property
+    @abstractmethod
+    def last_time_presence(self) -> datetime:
+        pass
+
+    def _notify_listeners(self):
+        [listener() for listener in self.__listeners]
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+
+
+class IpPresence(Presence):
+
+    def __init__(self, name: str, addr: str):
+        self.__is_running = True
+        self.addr = addr
+        self.__last_time_presence = datetime.now() - timedelta(days=365)
+        super().__init__(name, addr)
+        self.__check()
+
+    @property
+    def last_time_presence(self) -> datetime:
+        return self.__last_time_presence
+
+    def __check(self):
+        if self.ping() > 0:
+            self.__last_time_presence = datetime.now()
+            self._notify_listeners()
+
+    def ping(self, count: int = 10):
+        successful_pings = 0
+        for i in range(count):
+            ping_packet = IP(dst=self.addr) / ICMP()
+            response, _ = sr(ping_packet, timeout=2, verbose=False)
+            if response:
+                successful_pings += 1
+        return successful_pings
+
+    def start(self):
+        Thread(target=self.__check_loop, daemon=True).start()
+
+    def stop(self):
+        self.__is_running = False
+
+    def __check_loop(self):
+        while self.__is_running:
+            try:
+                self.__check()
+                sleep(9.03)
+            except Exception as e:
+                logging.warning("error occurred on check " + str(e))
+                sleep(3)
+
+
+class Presences(Presence):
+
+    def __init__(self, name: str, presences: List[Presence]):
+        self.__is_running = True
+        self.__presences = presences
+        [presence.add_listener(self.__notify) for presence in presences]
+        super().__init__(name, "all")
+
+    @property
+    def last_time_presence(self) -> datetime:
+        last_date_presence = datetime.now() - timedelta(days=365)
+        for presence in self.__presences:
+            if presence.last_time_presence < last_date_presence:
+                last_date_presence = presence.last_time_presence
+        return last_date_presence
+
+    def __notify(self):
+        self._notify_listeners()
---
+++ presence_webthing.py
@@ -0,0 +1,104 @@
+import sys
+import logging
+import tornado.ioloop
+from typing import Dict
+from webthing import (MultipleThings, Property, Thing, Value, WebThingServer)
+from presence import Presence, IpPresence, Presences
+
+
+
+
+class PresenceThing(Thing):
+
+    # regarding capabilities refer https://iot.mozilla.org/schemas
+    # there is also another schema registry http://iotschema.org/docs/full.html not used by webthing
+
+    def __init__(self, description: str, presence: Presence):
+        Thing.__init__(
+            self,
+            'urn:dev:ops:presence-1',
+            'presence_' + presence.name,
+            ['MultiLevelSensor'],
+            description
+        )
+        self.ioloop = tornado.ioloop.IOLoop.current()
+        self.presence = presence
+        self.presence.add_listener(self.on_value_changed)
+
+        self.name = Value(presence.name)
+        self.add_property(
+            Property(self,
+                     'name',
+                     self.name,
+                     metadata={
+                         'title': 'name',
+                         "type": "string",
+                         'description': 'the device name',
+                         'readOnly': True,
+                     }))
+
+        self.addr = Value(presence.addr)
+        self.add_property(
+            Property(self,
+                     'addr',
+                     self.addr,
+                     metadata={
+                         'title': 'addr',
+                         "type": "string",
+                         'description': 'the device address',
+                         'readOnly': True,
+                     }))
+
+        self.last_time_presence = Value(presence.last_time_presence.strftime("%Y-%m-%dT%H:%M"))
+        self.add_property(
+            Property(self,
+                     'last_time_presence',
+                     self.last_time_presence,
+                     metadata={
+                         'title': 'last_time_presence',
+                         "type": "string",
+                         'description': 'the last time presence ISO8601 string',
+                         'readOnly': True,
+                     }))
+
+
+    def on_value_changed(self):
+        self.ioloop.add_callback(self._on_value_changed)
+
+    def _on_value_changed(self):
+        self.last_time_presence.notify_of_external_update(self.presence.last_time_presence.strftime("%Y-%m-%dT%H:%M"))
+
+
+def run_server(description: str, port: int, name: str, name_address_map: Dict[str, str]):
+    if len(name_address_map) < 2:
+        presences = [IpPresence(name, name_address_map[dev_name]) for dev_name in name_address_map.keys()]
+    else:
+        presences = [IpPresence(name + "_" + dev_name, name_address_map[dev_name]) for dev_name in name_address_map.keys()]
+        presences = [Presences(name + "_all", presences)] + presences
+    shutters_tings = [PresenceThing(description, presence) for presence in presences]
+    server = WebThingServer(MultipleThings(shutters_tings, name), port=port, disable_host_validation=True)
+    try:
+        logging.info('starting the server http://localhost:' + str(port))
+        [presence.start() for presence in presences]
+        server.start()
+    except KeyboardInterrupt:
+        logging.info('stopping the server')
+        [presence.stop() for presence in presences]
+        server.stop()
+        logging.info('done')
+
+
+
+def parse_devices(config: str) -> Dict[str, str]:
+    name_address_map = {}
+    for device in config.split("&"):
+        name, address = device.split('=')
+        name_address_map[name.strip()] = address.strip()
+    return name_address_map
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s %(name)-20s: %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+    logging.getLogger('tornado.access').setLevel(logging.ERROR)
+    logging.getLogger('urllib3.connectionpool').setLevel(logging.WARNING)
+    run_server("description", int(sys.argv[1]), sys.argv[2], parse_devices(sys.argv[3]))
